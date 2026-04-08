c642fa7fe30f7aac597453e30c2fc5bcc207a809
biceps-yeon <du9904@gmail.com>
2026-04-08 11:05:03 +0900

    Uncomment and add stock symbols in tickers list

--- run_GithubAction.py
+++ run_GithubAction.py
@@ -57,24 +57,24 @@ if __name__ == "__main__":
         'name': 'KOSPI'
     }
     tickers = [
-        {'symbol': 'GC=F', 'name': 'COMESGoldFuture'},
+        # {'symbol': 'GC=F', 'name': 'COMESGoldFuture'},
         # {'symbol': 'BTC-USD', 'name': 'Bitcoin_USD'},
         {'symbol': 'ETH-USD', 'name': 'Ethereum_USD'},
         {'symbol': 'TLT', 'name': 'LongUSBond'},        
         # {'symbol': '^KS11', 'name': 'KOSPI'},
         {'symbol': '^KQ11', 'name': 'KOSDAQ'},
         {'symbol': '^GSPC', 'name': 'SNP500'},
-        # {'symbol': '^NDX', 'name': 'NSDQ100'},
+        {'symbol': '^NDX', 'name': 'NSDQ100'},
         # {'symbol': 'SCHD', 'name': 'SCHD'},
         {'symbol': 'TSLA', 'name': 'TESLA'},
         {'symbol': 'PLTR', 'name': 'PLTR'},
         {'symbol': 'AMD', 'name': 'AMD'},
         # {'symbol': 'ORCL', 'name': 'ORACLE'},
-        # {'symbol': 'MSFT', 'name': 'MICROSOFT'},
-        # {'symbol': 'NVDA', 'name': 'NVIDIA'},
-        {'symbol': 'GOOGL', 'name': 'Alphabet A'},
+        {'symbol': 'MSFT', 'name': 'MICROSOFT'},
+        {'symbol': 'NVDA', 'name': 'NVIDIA'},
+        # {'symbol': 'GOOGL', 'name': 'Alphabet A'},
         {'symbol': '005930.KS', 'name': 'SMSN'},
-        {'symbol': '000660.KS', 'name': 'SKHnx'},
+        # {'symbol': '000660.KS', 'name': 'SKHnx'},
         {'symbol': '005380.KS', 'name': 'Hyundai'}
         
     ]
