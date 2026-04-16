6b4347c13e874bbb5ba46adee51be87abccad8e7
biceps-yeon <du9904@gmail.com>
2026-04-16 10:58:37 +0900

    Add market configuration with tickers for KR and US

---
+++ config.py
@@ -0,0 +1,33 @@
+MarketConfig={
+  'KR': {
+    tickers = [
+  
+        {'symbol': '^KS11', 'name': 'KOSPI'},
+        {'symbol': '^KQ11', 'name': 'KOSDAQ'},
+        {'symbol': '005930.KS', 'name': 'SMSN'},
+        {'symbol': '000660.KS', 'name': 'SKHnx'},
+        {'symbol': '005380.KS', 'name': 'Hyundai'},
+        {'symbol': 'BTC-USD', 'name': 'Bitcoin_USD'},
+        {'symbol': 'ETH-USD', 'name': 'Ethereum_USD'},
+  
+    ]
+  },
+  'US': {
+    tickers = [
+    
+        {'symbol': 'GC=F', 'name': 'COMESGoldFuture'},
+        {'symbol': 'TLT', 'name': 'LongUSBond'},
+        {'symbol': '^GSPC', 'name': 'SNP500'},
+        {'symbol': '^NDX', 'name': 'NSDQ100'},
+        {'symbol': 'SCHD', 'name': 'SCHD'},
+        {'symbol': 'TSLA', 'name': 'TESLA'},
+        {'symbol': 'PLTR', 'name': 'PLTR'},
+        {'symbol': 'AMD', 'name': 'AMD'},
+        {'symbol': 'ORCL', 'name': 'ORACLE'},
+        {'symbol': 'MSFT', 'name': 'MICROSOFT'},
+        {'symbol': 'NVDA', 'name': 'NVIDIA'},
+        {'symbol': 'GOOGL', 'name': 'Alphabet A'},
+    
+    ]
+  }
+}
