8992bb3b36f510bb65de2b5ac0961d46990d6536
biceps-yeon <du9904@gmail.com>
2026-08-05 10:05:47 +0900

    Comment out Space X entry in config
    Space X don't have price before stock listing

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ MarketConfig={
         {'symbol': 'MSFT', 'name': 'MICROSOFT'},
         {'symbol': 'NVDA', 'name': 'NVIDIA'},
         {'symbol': 'GOOGL', 'name': 'Alphabet A'},
-        {'symbol': 'SPCX', 'name': 'Space X'},
+        #{'symbol': 'SPCX', 'name': 'Space X'},
     ]
   }
 }
